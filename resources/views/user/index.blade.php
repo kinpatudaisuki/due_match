6ea2f66134fa61289b11b5ca3f357be47e70231d
inoshun <mozduru2@gmail.com>
2024-10-03 14:52:42 +0900

    ユーザー一覧画面作成

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          {{ __('ユーザー一覧') }}
+      </h2>
+  </x-slot>
+
+  <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        @foreach ($users as $user)
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-4">
+            <div class="p-6 text-gray-900">
+                <p>{{$user->name}}</p>
+                <p>エリア：{{$user->area}}</p>
+            </div>
+        </div>
+        @endforeach
+      </div>
+  </div>
+</x-app-layout>
