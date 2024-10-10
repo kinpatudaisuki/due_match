7cb00cc094d86c29083def9c9fa05ab7d20eca11
inoshun <mozduru2@gmail.com>
2024-10-10 09:25:20 +0900

    ルームメンバー画像表示

--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -15,7 +15,22 @@
                 @foreach($rooms as $room)
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-4">
                         <a href="{{ route('room.show', $room->id) }}">
-                        メンバー：{{ $room->users->pluck('name')->implode(', ') }}
+                            メンバー：
+                            <div class="flex items-center space-x-4">
+                                @foreach($room->users as $user)
+                                    <div class="flex items-center">
+                                        @if($user->image)
+                                            <img src="{{ asset('storage/' . $user->image) }}" alt="{{ $user->name }}" class="w-10 h-10 rounded-full">
+                                        @else
+                                            <span class="w-10 h-10 flex items-center justify-center rounded-full bg-gray-200 text-gray-700">
+                                                {{ strtoupper(substr($user->name, 0, 1)) }}
+                                            </span>
+                                        @endif
+                                        <span class="ml-2">{{ $user->name }}</span>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </a>
                     </div>
                 @endforeach
             </ul>
