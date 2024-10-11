c0a8c5bc40f2fc44bae91743b2a645bb14e199f2
inoshun <mozduru2@gmail.com>
2024-10-11 14:07:11 +0900

    topページとログインページのレイアウト修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 
-    <body class="antialiased">
-        <div class="relative flex flex-col justify-center items-center min-h-screen bg-center bg-gray-200 font-semibold text-xl">
-            <x-application-logo width="35" class="pb-10"/>
-            <p>
-                GrayZoneへようこそ！
-            </p>
-            </p>
-                ここは発達障害のグレーゾーンの方や、
-            <p>
-            </p>
-                自分もグレーゾーンかもしれないと思っている方が、
-            <p>
-            </p>
-                悩みや愚痴を投稿する掲示板です。
-            <p>
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('post/index') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-blue-400 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">投稿一覧</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-blue-400 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">ログイン</a>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+    <style>
+        body {
+            font-family: 'Montserrat', sans-serif;
+            background-image: url('/img/background.png');
+            background-size: 600px 600px;
+            background-position: center;
+            background-repeat: no-repeat;
+        }
+    </style>
+</head>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-blue-400 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">会員登録</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-            <div class="font-semibold text-blue-400 hover:text-gray-900 dark:text-brue-400 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 mt-20">
-                <a href="{{route('contact.create')}}">お問い合わせ</a>
-            </div>
+<body class="antialiased">
+    <div class="relative flex flex-col justify-between min-h-screen font-semibold text-xl">
+        <div class="flex-grow flex flex-col justify-end items-center mb-4 text-white">
+
+            <p>
+                ここではデュエル・マスターズの
+            </p>
+            <p>
+                チーム戦に一緒に出るメンバーや、対戦相手とマッチングできます
+            </p>
         </div>
-    </body>
+
+        @if (Route::has('login'))
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                @auth
+                    <a href="{{ url('post/index') }}" class="ml-4 font-semibold text-white">ユーザー一覧</a>
+                @else
+                    <a href="{{ route('login') }}" class="ml-4 font-semibold text-blue-300">ログイン</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 font-semibold text-blue-300">会員登録</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+        <div class="font-semibold text-white text-center">
+            <a href="{{ route('contact.create') }}">お問い合わせ</a>
+        </div>
+    </div>
+</body>
 </html>
