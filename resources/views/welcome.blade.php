f3ca9f5f9d6db1dd7999879329159cc35bad9634
inoshun <mozduru2@gmail.com>
2024-10-24 09:12:22 +0900

    TOP画面レイアウト修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,39 +8,38 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    
-    <style>
-        body {
-            font-family: 'Montserrat', sans-serif;
-            background-image: url('/img/background.png');
-            background-size: 600px 600px;
-            background-position: center;
-            background-repeat: no-repeat;
-        }
-    </style>
+
 </head>
 
-<body class="antialiased">
-    <div class="relative flex flex-col justify-between min-h-screen font-semibold text-xl">
-        <div class="flex-grow flex flex-col justify-end items-center mb-4 text-white">
-
-            <p>
-                ここではデュエル・マスターズの
-            </p>
-            <p>
-                チーム戦に一緒に出るメンバーや、対戦相手とマッチングできます
-            </p>
+<body class="antialiased bg-blue-400">
+    <div class="relative flex flex-col justify-between min-h-screen text-xl">
+        <div class="flex-grow flex flex-col justify-end items-center text-white">
+            <img src="{{asset('img/logo.png')}}" class="w-48 mb-8">
+            <div class="mb-12 text-center">
+                <p>
+                    デュエマッチへようこそ！
+                </p>
+                <p>
+                    ここではデュエル・マスターズの
+                </p>
+                <p>
+                    チーム戦に一緒に出るメンバーや
+                </p>
+                <p>
+                    対戦相手とマッチングできます
+                </p>
+            </div>
         </div>
 
         @if (Route::has('login'))
-            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+            <div class="absolute top-0 right-0 p-6 text-right z-10">
                 @auth
-                    <a href="{{ url('user/index') }}" class="ml-4 font-semibold text-blue-300">ユーザー一覧</a>
+                    <a href="{{ url('user/index') }}" class="ml-4 font-semibold text-white">ユーザー一覧</a>
                 @else
-                    <a href="{{ route('login') }}" class="ml-4 font-semibold text-blue-300">ログイン</a>
+                    <a href="{{ route('login') }}" class="ml-4 font-semibold text-white">ログイン</a>
 
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 font-semibold text-blue-300">会員登録</a>
+                        <a href="{{ route('register') }}" class="ml-4 font-semibold text-white">会員登録</a>
                     @endif
                 @endauth
             </div>
