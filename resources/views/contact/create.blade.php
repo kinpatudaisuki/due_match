9eec856ec0df60312eeacc357d0082c0439cee79
inoshun <mozduru2@gmail.com>
2025-02-03 17:32:19 +0900

    レイアウト修正

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -1,17 +1,19 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center">
+            {{ __('お問い合わせ') }}
+        </h2>
+    </x-slot>
 
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-yellow-50 h-screen">
         <div class="mx-4 sm:p-8">
-            <h1 class="text-xl text-gray-700 font-semibold hover:underline cursor-pointer">
-                お問い合わせ
-            </h1>
-  
+
             @if(session('message'))
             <div class="text-red-600 font-bold">
                 {{session('message')}}
             </div>
             @endif
-  
+
             <form method="post" action="{{route('contact.store')}}" enctype="multipart/form-data">
             @csrf
                 <div class="md:flex items-center mt-8">
@@ -20,19 +22,19 @@
                         <input type="text" name="title" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="title" value="{{old('title')}}" placeholder="Enter Title">
                     </div>
                 </div>
-  
+
                 <div class="w-full flex flex-col">
                     <label for="body" class="font-semibold leading-none mt-4">本文</label>
                     <textarea name="body" class="w-auto py-2 border border-gray-300 rounded-md" id="body" cols="30" rows="10">{{old('body')}}</textarea>
                 </div>
-  
+
                 <div class="md:flex items-center">
                     <div class="w-full flex flex-col">
                         <label for="email" class="font-semibold leading-none mt-4">メールアドレス</label>
                         <input type="text" name="email" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="email" value="{{old('email')}}" placeholder="Enter Email">
                     </div>
                 </div>
-  
+
                 <!-- reCAPTCHA -->
                 <div class="mt-4">
                     <div class="g-recaptcha" data-sitekey="{{ env('RECAPTCHA_SITE_KEY') }}"></div>
@@ -44,7 +46,7 @@
             </form>
         </div>
     </div>
-  
+
     <!-- Google reCAPTCHAのスクリプト -->
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   </x-app-layout> 
